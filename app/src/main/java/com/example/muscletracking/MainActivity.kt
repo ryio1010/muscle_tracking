9aa8113f623c694d5849b553ba39713b4c71bd5e
RyoIwahara <riwahara@mini.local>
2021-12-28 22:54:28 +0900

    Create Login Page

--- app/src/main/java/com/example/muscletracking/MainActivity.kt
+++ app/src/main/java/com/example/muscletracking/MainActivity.kt
@@ -2,6 +2,8 @@ package com.example.muscletracking
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
