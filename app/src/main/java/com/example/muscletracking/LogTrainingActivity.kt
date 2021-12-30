6e8812caaf99ae040b71a8c482d2b0b781f965e3
RyoIwahara <riwahara@mini.local>
2021-12-30 22:04:20 +0900

    Modify LogTrainingActivity

--- app/src/main/java/com/example/muscletracking/LogTrainingActivity.kt
+++ app/src/main/java/com/example/muscletracking/LogTrainingActivity.kt
@@ -3,10 +3,13 @@ package com.example.muscletracking
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.view.LayoutInflater
 import android.view.View
-import android.widget.AdapterView
-import android.widget.ArrayAdapter
-import android.widget.Spinner
+import android.view.ViewGroup
+import android.widget.*
+import androidx.recyclerview.widget.DividerItemDecoration
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 
 class LogTrainingActivity : AppCompatActivity() {
     private val muscleGroups = mutableListOf(
@@ -52,6 +55,13 @@ class LogTrainingActivity : AppCompatActivity() {
         "oooo"
     )
 
+    private var _addTrainingMenu: MutableList<MutableMap<String, Any>> = mutableListOf()
+    private val _from = arrayOf("parts", "menu", "weight", "count")
+    private val _to =
+        intArrayOf(R.id.trainingParts, R.id.trainingMenu, R.id.trainingWeight, R.id.trainingCount)
+
+    private val addTrainingMenus: MutableList<MutableMap<String, Any>> = mutableListOf()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_log_training)
@@ -72,13 +82,45 @@ class LogTrainingActivity : AppCompatActivity() {
             ArrayAdapter(this, android.R.layout.simple_spinner_item, chestTrainingList)
         adapterTrainingMenus.setDropDownViewResource(android.R.layout.simple_spinner_item)
         spinnerTrainingMenus.adapter = adapterTrainingMenus
+
+        //set recycleview of training menus
+        val addTrainingContainer = findViewById<RecyclerView>(R.id.addTrainingContainer)
+        val layout = LinearLayoutManager(this@LogTrainingActivity)
+        addTrainingContainer.layoutManager = layout
+        val adapter = RecyclerListAdapter(addTrainingMenus)
+        addTrainingContainer.adapter = adapter
+        val decorator = DividerItemDecoration(this@LogTrainingActivity, layout.orientation)
+        addTrainingContainer.addItemDecoration(decorator)
+
+
+        // set listener of add button
+        val addButton = findViewById<Button>(R.id.btAddLog)
+        addButton.setOnClickListener{
+            val inputMuscleGroups =
+                findViewById<Spinner>(R.id.spinnerMuscleGroups).selectedItem.toString()
+            val inputTrainingMenu =
+                findViewById<Spinner>(R.id.spinnerTrainingMenus).selectedItem.toString()
+            val inputTrainingWeight = findViewById<TextView>(R.id.inputTrainingWeight).text.toString()
+            val inputTrainingCount = findViewById<TextView>(R.id.inputTrainingCount).text.toString()
+
+            if (inputTrainingWeight.isNotEmpty() and inputTrainingCount.isNotEmpty()){
+                var menu = mutableMapOf<String, Any>(
+                    "parts" to inputMuscleGroups,
+                    "menu" to inputTrainingMenu,
+                    "weight" to inputTrainingWeight,
+                    "count" to inputTrainingCount
+                )
+                addTrainingMenus.add(menu)
+                adapter.notifyItemInserted(addTrainingMenus.lastIndex)
+            }
+        }
+
     }
 
     private inner class spinnerSelectedMuscleGroupsListener : AdapterView.OnItemSelectedListener {
         override fun onItemSelected(parent: AdapterView<*>?, view: View?, position: Int, id: Long) {
             val spinner = parent as Spinner
             val selectedMuscleGroups = spinner!!.selectedItem.toString()
-            Log.d("test", "spinnertest" + selectedMuscleGroups)
 
             var trainingList = when (selectedMuscleGroups) {
                 "Chest" -> chestTrainingList
@@ -91,12 +133,57 @@ class LogTrainingActivity : AppCompatActivity() {
 
             val spinnerTrainingMenus = findViewById<Spinner>(R.id.spinnerTrainingMenus)
             val adapterTrainingMenus =
-                ArrayAdapter(this@LogTrainingActivity, android.R.layout.simple_spinner_item, trainingList)
+                ArrayAdapter(
+                    this@LogTrainingActivity,
+                    android.R.layout.simple_spinner_item,
+                    trainingList
+                )
             adapterTrainingMenus.setDropDownViewResource(android.R.layout.simple_spinner_item)
             spinnerTrainingMenus.adapter = adapterTrainingMenus
         }
 
         override fun onNothingSelected(p0: AdapterView<*>?) {
+        }
+    }
+
+    private inner class RecyclerListViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+        var _trainingParts: TextView
+        var _trainingMenu: TextView
+        var _trainingWeight: TextView
+        var _trainingCount: TextView
+
+        init {
+            _trainingParts = itemView.findViewById(R.id.trainingParts)
+            _trainingMenu = itemView.findViewById(R.id.trainingMenu)
+            _trainingWeight = itemView.findViewById(R.id.trainingWeight)
+            _trainingCount = itemView.findViewById(R.id.trainingCount)
+        }
+    }
+
+    private inner class RecyclerListAdapter(private val _listData: MutableList<MutableMap<String, Any>>) :
+        RecyclerView.Adapter<RecyclerListViewHolder>() {
+        override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RecyclerListViewHolder {
+            val inflater = LayoutInflater.from(this@LogTrainingActivity)
+            val view = inflater.inflate(R.layout.row, parent, false)
+            val holder = RecyclerListViewHolder(view)
+            return holder
+        }
+
+        override fun onBindViewHolder(holder: RecyclerListViewHolder, position: Int) {
+            val item = _listData[position]
+            val trainingParts = item["parts"] as String
+            val trainingMenu = item["menu"] as String
+            val trainingWeight = item["weight"] as String
+            val trainingCount = item["count"] as String
+
+            holder._trainingParts.text = trainingParts
+            holder._trainingMenu.text = trainingMenu
+            holder._trainingWeight.text = trainingWeight
+            holder._trainingCount.text = trainingCount
+        }
+
+        override fun getItemCount(): Int {
+            return _listData.size
         }
     }
 }
